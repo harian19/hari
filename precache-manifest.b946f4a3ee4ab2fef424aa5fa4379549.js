b4fb0e676aa1aeb5437c9f12bb82179c84c27fe6
Hari <harianantharaman19@gmail.com>
2022-09-02 15:16:42 -0600

    Updates

---
+++ precache-manifest.b946f4a3ee4ab2fef424aa5fa4379549.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374034421aac30e498542bca8504800a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3290735281b9062905a",
+    "url": "/static/css/main.09a61061.chunk.css"
+  },
+  {
+    "revision": "200b60058a4313cd133d",
+    "url": "/static/js/2.a8fb2994.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.a8fb2994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3290735281b9062905a",
+    "url": "/static/js/main.bd075406.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "1a157a52d76813bd407f2fd6e43ae1a2",
+    "url": "/static/media/Avatar.1a157a52.svg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/static/media/react.f370c1e0.png"
+  }
+]);
